af7fd8e10876ee9b68a0eaf20435e1f36be1bc09
Jeff Jetton <jeff.jetton@gmail.com>
2020-05-03 09:39:23 -0500

    Add win/loss behavior

--- shut.asm
+++ shut.asm
@@ -6,6 +6,7 @@
 ; Written for the dasm assembler, but should assemble under others
 ; with a few tweaks here and there.
 
+; 1_3______  with a single-die roll of 4???
 
 
         processor 6502
@@ -19,12 +20,11 @@ ECHO    equ $FFEF           ; WozMon routine to display register A char
 WOZMON  equ $FF1F           ; Entry point back to WozMonitor (It's own GETLINE)
 CR      equ $0D             ; Carriage return ASCII value
 BACKSP  equ "_"             ; Yup. This is what the backspace key generates.
-MAXBUFF equ 200             ; Size of input text buffer (if buffer is maxed out,
-                            ; we'll automatically add a CR to the end, so total
-                            ; size might be as much as MAXBUFF + 1)
+WIDTH   equ 40              ; Screen width, in characters
 SHUT    equ $2F             ; Value to represent "shut" in the DOORS array
                             ; When converted to ASCII in the same way as the 
-                            ; other digits, this will display as an underscore
+                            ; other digits, this will display as an underscore.
+                            ; (Also makes the game over check efficient)
 
 
         ; Zero-page variables
@@ -34,7 +34,6 @@ SHUT    equ $2F             ; Value to represent "shut" in the DOORS array
 
 DOORS   ds 9    ; The state of the 9 "doors"
 CHOICES ds 9    ; Keeps track of unique door numbers picked by the player
-                ; Also used as a temp holding area for calculating win status
 CYCOFF  ds 1    ; A constantly-cycled (during key polling) offset into the
                 ; die results table.  Creates "random" die rolls (sort of)
                 ; without actually futzing around with a PRNG.
@@ -65,7 +64,7 @@ BUFFER  ds 1    ; Beginning of text buffer (ensure that this is defined last!)
         sta TXTLO
         lda #>TXT_WELCOME   ; Store high byte of text data location
         sta TXTHI
-        jsr PRINTXT         ; Call generic print function
+        jsr PRTEXT2         ; Call version of print function that adds CRs
         
         ; Get and handle answer to instructions question
         jsr GETYN
@@ -74,7 +73,8 @@ BUFFER  ds 1    ; Beginning of text buffer (ensure that this is defined last!)
         sta TXTLO
         lda #>TXT_INSTRUCT
         sta TXTHI
-        jsr PRINTXT
+        jsr PRTEXT2
+        
         
         ; Initialize game with all "doors" open
 NEWGAME ldx #8
@@ -85,16 +85,15 @@ NEWGAME ldx #8
         sbc #1
         dex
         bpl .initlp
-        lda #45
+        lda #$45        ; Score starts at BCD 45 (total of all digits)
         sta SCORE
         
         
         ; Print game state
-PRINGAM jsr NEWLINE
-        jsr NEWLINE
+PRINGAM jsr TWOLINE
         ldx #0
 .prgmlp lda DOORS,x
-        jsr ECHOASC     ; Display the integer as ASCII
+        jsr ECHOINT     ; Display the door value
         inx
         cpx #9
         bne .prgmlp
@@ -102,122 +101,129 @@ PRINGAM jsr NEWLINE
         ; Check to see if we've won (no doors open--i.e. all shut)
         lda NUMOPEN
         bne GETDICE
-        
-        ; TODO: Finish this bit. Show a proper message.
-        jsr NEWLINE
-        jsr NEWLINE
-        lda #":"
-        jsr ECHO
-        lda #"-"
-        jsr ECHO
-        lda #")"
-        jsr ECHO
-        jsr NEWLINE
-        jmp WOZMON
+        ; We've won!!!!
+        jsr TWOLINE
+        jsr TWOLINE
+        lda #"*"
+        ldx #40
+        jsr PRINREP
+        lda #" "
+        ldx #92
+        jsr PRINREP
+        lda #<TXT_WINNER
+        sta TXTLO
+        lda #>TXT_WINNER
+        sta TXTHI
+        jsr PRTEXT
+        lda #"*"
+        ldx #40
+        jsr PRINREP
+        jsr TWOLINE
+        jmp REPLAY
+
         
 GETDICE ; Get and print dice roll (and also calculate dice total)
         lda #<TXT_ROLL      ; Label for dice roll results
         sta TXTLO
         lda #>TXT_ROLL
         sta TXTHI
-        jsr PRINTXT
+        jsr PRTEXT2
         ldx CYCOFF          ; Get current dice value
         lda DICE,x
         sta CURROLL         ; Store as raw BCD for now...
         
         ; TODO: Use some wozmon routines for some of this?
-        ; TODO: Only one die if open total <= 6?
         lsr                 ; Isolate first die number
         lsr
         lsr
         lsr
         sta ROLLTOT         ; Save as first part of total
-        jsr ECHOASC         ; Display first die text
+        jsr ECHOINT         ; Display first die text
+        lda SCORE           ; Is the total of remaining (unshut)
+        cmp #7              ;   digits 6 or less? If so,
+        bmi .skpdie         ;   only use the first die
         lda #<TXT_AND       ; Print " & "
         sta TXTLO
         lda #>TXT_AND
         sta TXTHI
-        jsr PRINTXT
-        lda CURROLL         ; Isolate second die value
-        and #$0F
-        tax                 ; Remember it in X for a bit... TODO: Really?
+        jsr PRTEXT
+        lda CURROLL         ; Isolate value of
+        and #$0F            ;   the second die
+        tax                 ; Remember it in X
+        jsr ECHOINT         ;   display it (this destroys A)
+        txa                 ;   then bring it back
         clc                 ; Add die value to total
         adc ROLLTOT
         sta ROLLTOT
-        txa                 ; Bring the die value back
-        jsr ECHOASC         ; Display second die value
-        jsr NEWLINE
-        jsr NEWLINE
-        
+.skpdie jsr TWOLINE
+
         
         
 CHKDONE ; Check to see if there even is any valid move or if game over
-        ; TODO:  ???
-        ; TODO: Better backwards?
-        ; TODO: Better if we just operate out of DOORS directly?
-        ; First, load remaining doors into an array (without gaps)
-        ldx #0
-        ldy #0
-.loadlp lda DOORS,x
-        cmp #SHUT
-        beq .skipld
-        sta CHOICES,y
-        iny
-.skipld inx
-        cpx #9
-        bne .loadlp
         ; Work backwards through array. Y is our "current top" index
         ; X will be the "moving left from top" index. A is working total.
-        dey             ; Undo that last iny above
-.setx   tya
+        ldy #8
+.newtop lda DOORS,y     ; Is the current "top"
+        cmp #SHUT       ;   digit already shut?
+        beq .dectop     ;   If so, skip it
+        tya
         tax             ; x = y
         lda ROLLTOT
-.trysub cmp CHOICES,x   ; Can current door be subtracted from working total?
-        bmi .skpsub     ; Don't subtract if not
-        sec
-        sbc CHOICES,x
+.trysub cmp DOORS,x     ; Can current door be subtracted from working total?
+        bmi .skpsub     ; Don't subtract if not. Note that the "shut" value
+        sec             ; is large enought to always branch, which is handy!
+        sbc DOORS,x
         beq GETMOVE     ; Down to zero? Valid move exists--game not over yet!
 .skpsub dex             ; Bump index to the left
         bpl .trysub     ; If not past the end of array, try another subtraction
-        dey             ; Bump the top to the left
-        bpl .setx       ; Top not past end? Reset X & A and try again.
+.dectop dey             ; Bump the top to the left
+        bpl .newtop     ; Top not past end? Reset X & A and try again.
         
-        ; Algoright failed on 1  4  789
-        ; with roll of 1 & 5
+
+GAMEOVER    ; No more moves. Display "game over" message
+        lda #<TXT_GAMEOVER
+        sta TXTLO
+        lda #>TXT_GAMEOVER
+        sta TXTHI
+        jsr PRTEXT
         
-        ; TODO: fix this to make a proper game over
-        jsr NEWLINE
-        jsr NEWLINE
-        lda #"%"
-        jsr ECHO
-        jsr ECHO
-        jsr ECHO
-        jsr ECHO
-        jsr NEWLINE
-        jsr NEWLINE
-        lda SCORE
+        ; Score display is different if only one digit left
+        lda NUMOPEN
+        cmp #1
+        beq .onedig
+        
+        ; Standard score message
+        jsr ECHOINT             ; Print num digits
+        lda #<TXT_SCOREMULT     ; Print score "label" text
+        sta TXTLO
+        lda #>TXT_SCOREMULT
+        sta TXTHI
+        jsr PRTEXT
+        lda SCORE               ; Tack score on end
         jsr PRBYTE
-        jsr NEWLINE
+        jmp REPLAY
+        
+        ; Score message if one digit left
+.onedig lda #<TXT_SCOREONE
+        sta TXTLO
+        lda #>TXT_SCOREONE
+        sta TXTHI
+        jsr PRTEXT
+        jmp REPLAY
         
         
-        
-        jmp WOZMON
-        
-        
-        ; Get player's door choice(s)
-GETMOVE jsr GETLINE
-        
-        ; Process the input line.  TODO: Could this go backwards from the end?
-        ldx #8          ; First clear out the bytes that track unique...
-        lda #0          ; ...door number choices for this turn
+GETMOVE ldx #8          ; Zero out the bytes that track unique
+        lda #0          ;   door number choices for this turn
 .clrlp  sta CHOICES,x
         dex
         bpl .clrlp
-        inx             ; Now prepare to move through input buffer
-.chkcr  lda BUFFER,x
-        inx
-        cmp #CR         ; Have we reached the end of the buffer?
-        beq CHKMOVE
+        jsr GETLINE
+        
+        ; Process the input line.
+        ; Note that, after GETLINE, X points to one past end of buffer text
+.chkcr  dex             ; Move x one char to the left
+        bmi CHKMOVE     ; If past beginning of 128-char buffer, we're done
+        lda BUFFER,x    ; Otherwise, put char in A
         sec             ; Is this a valid digit, 1-9?...
         sbc #"1"        ;   Converts "1" to 0 and "9" to 8
         cmp #9          ;   If we're not less than 9, not a valid door number
@@ -226,7 +232,6 @@ GETMOVE jsr GETLINE
         adc #1          ; Convert A from offset to value (note that C is clear)
         sta CHOICES,y   ; Set unique choice number byte to value of that choice
         jmp .chkcr
-        
         
         
 CHKMOVE ; Is the move valid? (Didn't pick an already-shut door)
@@ -259,10 +264,12 @@ SHUTEM  ; Go back through and set chosen doors to "shut"
         ldx #8
 .doorlp lda CHOICES,x
         beq .nextdr
+        sed             ; Prepare for BCD operation...
         lda SCORE       ; Decrease the score
         sec             ;   by the value
         sbc CHOICES,x   ;   of the shut
         sta SCORE       ;   door
+        cld             ; Back to decimal operation
         dec NUMOPEN     ; Update count of open doors
         lda #SHUT       ; Mark the door as being shut
         sta DOORS,x
@@ -270,34 +277,39 @@ SHUTEM  ; Go back through and set chosen doors to "shut"
         bpl .doorlp
         
         ; That's one turn down...
+        
+        ; TEMP fake a win...
+        ; lda #0
+        ; sta NUMOPEN
+        
         jmp PRINGAM
 
 
 
-        ; Game over...
-REPLAY  lda #<TXT_REPLAY    ; Prompt for playing another round
+        ; Prompt for playing another round
+REPLAY  lda #<TXT_REPLAY
         sta TXTLO
         lda #>TXT_REPLAY
         sta TXTHI
-        jsr PRINTXT
+        jsr PRTEXT2
         jsr GETYN           ; Get valid Y or N input
-        bne ENDGAME
+        bne GOODBYE
         jmp NEWGAME
         
-ENDGAME lda #<TXT_BYE
+GOODBYE lda #<TXT_BYE       ; TODO: Save about 19 bytes by getting rid of this
         sta TXTLO
         lda #>TXT_BYE
         sta TXTHI
-        jsr PRINTXT
+        jsr PRTEXT2
         jmp WOZMON          ; Return to WozMonitor
 
 
 
 ; Subroutines   ***************************************************************
 ; TODO: Implement ESCape
-; TODO: Use woz method to limit buffer size if bytes still need to be shaved
 GETLINE SUBROUTINE  ; Read input into the buffer (also cycles dice offset)
         ldx #0          ; Register X is our current buffer offset
+        
         ; Dice cycling...
 .getkey dec CYCOFF      ; Bump the die results offset down one byte
         bmi .rstoff     ; If we've gone past zero, reset back to the end
@@ -306,31 +318,30 @@ GETLINE SUBROUTINE  ; Read input into the buffer (also cycles dice offset)
 .rstoff lda #35         ; 2 cycles (plus extra cycle from the taken branch)
         sta CYCOFF      ; 3 cycles
         ; ...end of dice cycling
+        
 .chkpia lda KBDCR       ; Check PIA for keyboard input
         bpl .getkey     ; Loop if A is "positive" (bit 7 low... no key ready)
         lda KBD         ; Get the keyboard character
         and #%01111111  ; Clear bit 7, which is always set for some reason
         jsr ECHO        ; Always echo what the user just typed
-        sta BUFFER,x    ; Store it in the buffer too
-        cmp #CR         ; Did they hit "return"?
-        bne .chkbs
-        rts
+        
 .chkbs  cmp #BACKSP     ; Did they hit backspace?
-        bne .next
+        bne .bufstr
         dex             ; Move buffer pointer back
-        dex             ; TODO:  Move where we do this so we never write the _
-        ;  to the buffer and only have to do one dex
-        bmi GETLINE     ; Reset the whole shebang if we backspaced all the way 
+        bmi GETLINE     ; Reset the whole shebang if we backspaced all the way
+        jmp .getkey     ; Otherwise, just get next key input
+             
+.bufstr sta BUFFER,x    ; Store key in the buffer
+        cmp #CR         ; Did they hit "return"?
+        beq .done
+
 .next   inx             ; Next buffer offset please
-        cpx #MAXBUFF
-        bne .getkey     ; As long as there's room in the buffer, get another key
-        lda #CR
-        sta BUFFER,x    ; Otherwise, tack on a CR and peace out
+        bpl .getkey     ; Wozmon-style 128-character limit. Return if full.
 .done   rts
 
 
-
-PRINTXT SUBROUTINE  ; Put string pointer in TXTLO & TXTHI before calling
+PRTEXT2 jsr TWOLINE     ; Call this to put two CRs before what you're printing
+PRTEXT  SUBROUTINE      ; Put string pointer in TXTLO & TXTHI before calling
         ldy #0          ; Y is the offset within the string
 .loop   lda (TXTLO),Y   ; Load A with whatever's at pointer + Y
         beq .end        ; If char data is zero, that's the end
@@ -340,13 +351,13 @@ PRINTXT SUBROUTINE  ; Put string pointer in TXTLO & TXTHI before calling
 .end    rts             ; Note that we've destoyed A & Y
 
 
-; TODO: put a TWOLINE routine here to replace two newlines in a row
-NEWLINE ; Just print out a newline (destroys A)
-        lda #$0D
+
+TWOLINE lda #CR
+        jsr ECHO
         jmp ECHO
 
 
-ECHOASC ; Convert integer in A to ASCII equivalent before calling ECHO
+ECHOINT ; Convert integer in A to ASCII equivalent before calling ECHO
         clc
         adc #"0"
         jmp ECHO
@@ -371,46 +382,47 @@ PRINERR ; Displays a standard input error message
         sta TXTLO
         lda #>TXT_BADINPUT
         sta TXTHI
-        jmp PRINTXT
+        jmp PRTEXT2
 
+
+PRINREP SUBROUTINE  ; Prints whatever's in A for X times
+.loop   jsr ECHO
+        dex
+        bne .loop
+        rts
+        
 
 
 ; Stored data   ****************************************************************
 
 TXT_WELCOME
-        .byte $0D
-        .byte $0D
-        dc "     SHUT THE BOX -- BY JEFF JETTON"
-        .byte $0D
-        .byte $0D
-        dc "           (WORK IN PROGRESS)"
-        .byte $0D
+        dc "SHUT THE BOX - BY JEFF JETTON"
         .byte $0D
         .byte $0D
         .byte $0D
         dc "INSTRUCTIONS (Y/N)? "
         .byte $00
 
-TXT_INSTRUCT
-        .byte $0D
-        .byte $0D
+TXT_INSTRUCT  ; TODO: remove periods if needed!
         dc "ENTER ONE OR MORE AVAILABLE DIGITS TO"
         .byte $0D
-        dc "REMOVE THEM.  CHOICE OF DIGIT(S) MUST"
+        dc "REMOVE THEM.  DIGIT(S) PICKED MUST HAVE "
+        dc "SAME TOTAL AS DICE."
         .byte $0D
-        dc "HAVE THE SAME TOTAL AS YOUR DICE ROLL."
+        .byte $0D
+        dc "ONLY 1 DIE ROLLED IF REMAINING DIGITS"
+        .byte $0D
+        dc "TOTAL 6 OR LESS"
         .byte $0D
         .byte $0D
-        dc "GAME ENDS IF NO VALID CHOICE REMAINS."
+        dc "GAME ENDS IF NO VALID CHOICE LEFT"
+        .byte $0D
         .byte $0D
         dc "REMOVE EVERY DIGIT TO WIN!"
         .byte $0D
-        .byte $00  
-
+        .byte $00
 
 TXT_ROLL
-        .byte $0D
-        .byte $0D
         dc "YOU ROLL "
         .byte $00
         
@@ -419,20 +431,45 @@ TXT_AND
         .byte $00        
 
 TXT_BADINPUT
-        .byte $0D
         dc "INVALID CHOICE.  TRY AGAIN: "
         .byte $00
         
+TXT_WINNER
+        dc "CONGRATULATIONS!"
+        .byte $0D
+        .byte $0D
+        .byte $0D
+        .byte $0D
+        dc "          YOU'VE SHUT THE BOX!"
+        .byte $0D
+        .byte $0D
+        .byte $0D
+        byte $00
+
+TXT_GAMEOVER
+        dc "NO MORE MOVES"
+        .byte $0D
+        .byte $0D
+        .byte $0D
+        .byte $00
+
+TXT_SCOREMULT
+        dc " DIGITS LEFT TOTALING "
+        .byte $00
+                
+TXT_SCOREONE
+        dc "OOOH... SO CLOSE!"
+        .byte $0D
+        .byte $0D
+        dc "JUST ONE LEFT!"
+        .byte $00            
         
 TXT_REPLAY
-        .byte $0D
         .byte $0D
         dc "PLAY AGAIN (Y/N)? "
         .byte $00
 
 TXT_BYE
-        .byte $0D
-        .byte $0D
         dc "BYE!"
         .byte $0D
         .byte $0D
@@ -440,6 +477,7 @@ TXT_BYE
 
         ; TODO if I can code a 16-bit prng in less than 36 bytes...
         ;      or create a table in memory in less than 36 bytes
+        ; Or mix up digit two better
 DICE    hex 11 12 13 14 15 16
         hex 41 42 43 44 45 46
         hex 31 32 33 34 35 36
